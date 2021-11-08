e7403e13eca6c71ce18e0bbf90019ae8bfc72fcb
tiyool0928 <cho970928@naver.com>
2021-11-08 19:46:47 +0900

    upload files

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,200 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class GameManager : MonoBehaviour
+{
+    public string[] enemyObjs;
+    public Transform[] spawnPoints;
+
+    public float nextSpawnDelay;
+    public float curSpawnDelay;
+
+    public GameObject player;
+    public Text scoreText;
+    public Image[] lifeImage;
+    public Image[] boomImage;
+    public GameObject gameOverSet;
+    public ObjectManager objectManager;
+
+    public List<Spawn> spawnList;
+    public int spawnIndex;
+    public bool spawnEnd;
+
+    void Awake()
+    {
+        spawnList = new List<Spawn>();
+        enemyObjs = new string[]{ "EnemyS", "EnemyM", "EnemyL", "EnemyB" };
+        ReadSpawnFile();
+    }
+
+    void ReadSpawnFile()
+    {
+        // 변수 초기화
+        spawnList.Clear();
+        spawnIndex = 0;
+        spawnEnd = false;
+
+        // 리스폰 파일 받기
+        TextAsset textFile = Resources.Load("Stage 0") as TextAsset;
+        StringReader stringReader = new StringReader(textFile.text);    //파일 내의 문자열 데이터 읽기 클래스
+
+        while (stringReader != null)
+        {
+            string line = stringReader.ReadLine();  //텍스트 데이터를 한 줄씩 반환
+            Debug.Log(line);
+
+            if (line == null)
+                break;
+
+            // 리스폰 데이터 생성
+            Spawn spawnData = new Spawn();
+            spawnData.delay = float.Parse(line.Split(',')[0]);  //구분자
+            spawnData.type = line.Split(',')[1];
+            spawnData.point = int.Parse(line.Split(',')[2]);
+            spawnList.Add(spawnData);
+        }
+
+        //파일 닫기
+        stringReader.Close();
+
+        //  첫번째 스폰 딜레이
+        nextSpawnDelay = spawnList[0].delay;
+    }
+
+    void Update()
+    {
+        curSpawnDelay += Time.deltaTime;
+
+        if(curSpawnDelay > nextSpawnDelay && !spawnEnd)
+        {
+            SpawnEnemy();
+            curSpawnDelay = 0;
+        }
+
+        //UI 업데이트
+        Player playerLogic = player.GetComponent<Player>();
+        scoreText.text = string.Format("{0:n0}", playerLogic.score); // {0:n0} 세자리 마다 쉼표로 나눠주는 숫자 양식
+    }
+
+    void SpawnEnemy()
+    {
+        int enemyIndex = 0;
+        switch(spawnList[spawnIndex].type)
+        {
+            case "S":
+                enemyIndex = 0;
+                break;
+            case "M":
+                enemyIndex = 1;
+                break;
+            case "L":
+                enemyIndex = 2;
+                break;
+            case "B":
+                enemyIndex = 3;
+                break;
+        }
+        int enemyPoint = spawnList[spawnIndex].point;
+        GameObject enemy = objectManager.MakeObj(enemyObjs[enemyIndex]);
+        enemy.transform.position = spawnPoints[enemyPoint].position;
+
+        Rigidbody2D rigid = enemy.GetComponent<Rigidbody2D>();
+        Enemy enemyLogic = enemy.GetComponent<Enemy>();
+        enemyLogic.player = player;
+        enemyLogic.objectManager = objectManager;
+        if(enemyIndex == 0)
+        {       //EnemyS일 경우 플레이어 유도 공격
+            enemy.transform.Rotate(player.transform.position);
+            Vector3 dirVec = player.transform.position - enemy.transform.position;
+            rigid.AddForce(dirVec.normalized * 5, ForceMode2D.Impulse);
+        }
+        else
+        {
+            if (enemyPoint == 6 || enemyPoint == 7) //right spawn
+            {
+                enemy.transform.Rotate(Vector3.back * 90);
+                rigid.velocity = new Vector2(enemyLogic.speed * (-1), 1);
+            }
+
+            else if (enemyPoint == 8 || enemyPoint == 9) //left spawn
+            {
+                enemy.transform.Rotate(Vector3.forward * 90);
+                rigid.velocity = new Vector2(enemyLogic.speed, -1);
+            }
+
+            else //front spawn
+            {
+                rigid.velocity = new Vector2(0, enemyLogic.speed * (-1));
+            }
+        }
+
+        //  리스폰 인덱스 증가
+        spawnIndex++;
+        if(spawnIndex == spawnList.Count)
+        {
+            spawnEnd = true;
+            return;
+        }
+
+        //  다음 리스폰 딜레이 갱신
+        nextSpawnDelay = spawnList[spawnIndex].delay;
+    }
+
+    public void UpdateLifeIcon(int life)
+    {
+        //life UI 초기화
+        for(int index=0; index < 3; index++)
+        {
+            lifeImage[index].color = new Color(1, 1, 1, 0);
+        }
+
+        //life UI 활성화
+        for(int index=0; index<life; index++)
+        {
+            lifeImage[index].color = new Color(1, 1, 1, 1);
+        }
+    }
+
+    public void UpdateBoomIcon(int boom)
+    {
+        //boom UI 초기화
+        for (int index = 0; index < 3; index++)
+        {
+            boomImage[index].color = new Color(1, 1, 1, 0);
+        }
+
+        //boom UI 활성화
+        for (int index = 0; index < boom; index++)
+        {
+            boomImage[index].color = new Color(1, 1, 1, 1);
+        }
+    }
+
+    public void RespawnPlayer()
+    {
+        Invoke("RespawnPlayerExe", 2f);
+    }
+
+    void RespawnPlayerExe()
+    {
+        player.transform.position = Vector3.down * 3.5f;
+        player.SetActive(true);
+
+        Player playerLogic = player.GetComponent<Player>();
+        playerLogic.isHit = false;
+    }
+
+    public void GameOver()
+    {
+        gameOverSet.SetActive(true);
+    }
+
+    public void GameRetry()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,328 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public bool isTouchTop;
+    public bool isTouchBottom;
+    public bool isTouchLeft;
+    public bool isTouchRight;
+
+    public int life;
+    public int score;
+    public float speed;
+    public int maxPower;
+    public int power;
+    public int maxBoom;
+    public int boom;
+    public float maxShotDelay;
+    public float curShotDelay;
+
+    public GameObject bulletObjA;
+    public GameObject bulletObjB;
+    public GameObject boomEffect;
+
+    public GameManager gameManager;
+    public ObjectManager objectManager;
+    public bool isHit;
+    public bool isBoomTime;
+
+    Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Move();
+        Fire();
+        Boom();
+        Reload();
+    }
+
+    void Move()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        if ((isTouchRight && h == 1) || (isTouchLeft && h == -1))
+            h = 0;
+        float v = Input.GetAxisRaw("Vertical");
+        if ((isTouchTop && v == 1) || (isTouchBottom && v == -1))
+            v = 0;
+        Vector3 curPos = transform.position;
+        Vector3 nextPos = new Vector3(h, v, 0) * speed * Time.deltaTime;
+        //deltaTime -> 이동속도 일정
+
+        transform.position = curPos + nextPos;
+
+        if (Input.GetButtonDown("Horizontal") ||
+           Input.GetButtonUp("Horizontal"))
+        {
+            anim.SetInteger("Input", (int)h);
+        }
+    }
+
+    void Fire()
+    {
+        if (!Input.GetButton("Fire1"))
+            return;
+
+        if (curShotDelay < maxShotDelay)
+            return;
+
+        switch (power)
+        {
+            case 1:
+                GameObject bullet = objectManager.MakeObj("BulletPlayerA");
+                bullet.transform.position = transform.position;
+                //위치 매개변수는 플레이어 transform을 이용
+                Rigidbody2D rigid = bullet.GetComponent<Rigidbody2D>();
+                rigid.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                break;
+            case 2:
+                GameObject bulletR = objectManager.MakeObj("BulletPlayerA");
+                bulletR.transform.position = transform.position + Vector3.right * 0.1f;
+                GameObject bulletL = objectManager.MakeObj("BulletPlayerA");
+                bulletL.transform.position = transform.position + Vector3.left * 0.1f;
+                //위치 매개변수는 플레이어 transform을 이용
+                Rigidbody2D rigidR = bulletR.GetComponent<Rigidbody2D>();
+                Rigidbody2D rigidL = bulletL.GetComponent<Rigidbody2D>();
+                rigidR.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                rigidL.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                break;
+            case 3:
+                GameObject bulletRR = objectManager.MakeObj("BulletPlayerA");
+                bulletRR.transform.position = transform.position + Vector3.right * 0.25f;
+                GameObject bulletCC = objectManager.MakeObj("BulletPlayerA");
+                bulletCC.transform.position = transform.position;
+                GameObject bulletLL = objectManager.MakeObj("BulletPlayerA");
+                bulletLL.transform.position = transform.position + Vector3.left * 0.25f;
+                //위치 매개변수는 플레이어 transform을 이용
+                Rigidbody2D rigidRR = bulletRR.GetComponent<Rigidbody2D>();
+                Rigidbody2D rigidCC = bulletCC.GetComponent<Rigidbody2D>();
+                Rigidbody2D rigidLL = bulletLL.GetComponent<Rigidbody2D>();
+                rigidRR.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                rigidCC.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                rigidLL.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                break;
+            case 4:
+                GameObject bulletRRR = objectManager.MakeObj("BulletPlayerB");
+                bulletRRR.transform.position = transform.position + Vector3.right * 0.5f;
+                GameObject bulletCCC = objectManager.MakeObj("BulletPlayerB");
+                bulletCCC.transform.position = transform.position;
+                GameObject bulletLLL = objectManager.MakeObj("BulletPlayerB");
+                bulletLLL.transform.position = transform.position + Vector3.left * 0.5f;
+                //위치 매개변수는 플레이어 transform을 이용
+                Rigidbody2D rigidRRR = bulletRRR.GetComponent<Rigidbody2D>();
+                Rigidbody2D rigidCCC = bulletCCC.GetComponent<Rigidbody2D>();
+                Rigidbody2D rigidLLL = bulletLLL.GetComponent<Rigidbody2D>();
+                rigidRRR.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                rigidCCC.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                rigidLLL.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                break;
+        }
+
+        curShotDelay = 0;
+    }
+
+    void Reload()
+    {
+        curShotDelay += Time.deltaTime;
+    }
+
+    void Boom()
+    {
+        if (!Input.GetButton("Fire2"))
+            return;
+
+        if (isBoomTime)
+            return;
+
+        if (boom == 0)
+            return;
+
+        boom--;
+        isBoomTime = true;
+        gameManager.UpdateBoomIcon(boom);
+
+        boomEffect.SetActive(true);
+        Invoke("OffBoomEffect", 2f);
+
+        GameObject[] enemiesB = objectManager.GetPool("EnemyB");
+        GameObject[] enemiesL = objectManager.GetPool("EnemyL");
+        GameObject[] enemiesM = objectManager.GetPool("EnemyM");
+        GameObject[] enemiesS = objectManager.GetPool("EnemyS");
+
+        for (int index = 0; index < enemiesB.Length; index++)
+        {
+            if (enemiesB[index].activeSelf)
+            {
+                Enemy enemyLogic = enemiesL[index].GetComponent<Enemy>();
+                enemyLogic.OnHit(1000);
+            }
+        }
+
+        for (int index = 0; index < enemiesL.Length; index++)
+        {
+            if (enemiesL[index].activeSelf)
+            {
+                Enemy enemyLogic = enemiesL[index].GetComponent<Enemy>();
+                enemyLogic.OnHit(1000);
+            }
+        }
+
+        for (int index = 0; index < enemiesM.Length; index++)
+        {
+            if (enemiesM[index].activeSelf)
+            {
+                Enemy enemyLogic = enemiesM[index].GetComponent<Enemy>();
+                enemyLogic.OnHit(1000);
+            }
+        }
+
+        for (int index = 0; index < enemiesS.Length; index++)
+        {
+            if (enemiesS[index].activeSelf)
+            {
+                Enemy enemyLogic = enemiesS[index].GetComponent<Enemy>();
+                enemyLogic.OnHit(1000);
+            }
+        }
+
+        GameObject[] bulletsEnemyA = objectManager.GetPool("BulletEnemyA");
+        GameObject[] bulletsEnemyB = objectManager.GetPool("BulletEnemyB");
+        GameObject[] bulletsBossA = objectManager.GetPool("BulletBossA");
+        GameObject[] bulletsBossB = objectManager.GetPool("BulletBossB");
+        for (int index = 0; index < bulletsEnemyA.Length; index++)
+        {
+            if(bulletsEnemyA[index].activeSelf)
+            {
+                bulletsEnemyA[index].SetActive(false);
+            }
+        }
+
+        for (int index = 0; index < bulletsEnemyB.Length; index++)
+        {
+            if (bulletsEnemyB[index].activeSelf)
+            {
+                bulletsEnemyB[index].SetActive(false);
+            }
+        }
+
+        for (int index = 0; index < bulletsBossA.Length; index++)
+        {
+            if (bulletsBossA[index].activeSelf)
+            {
+                bulletsBossB[index].SetActive(false);
+            }
+        }
+
+        for (int index = 0; index < bulletsBossB.Length; index++)
+        {
+            if (bulletsBossB[index].activeSelf)
+            {
+                bulletsBossB[index].SetActive(false);
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Border")
+        {
+            switch(collision.gameObject.name)
+            {
+                case "Top":
+                    isTouchTop = true;
+                    break;
+                case "Bottom":
+                    isTouchBottom = true;
+                    break;
+                case "Left":
+                    isTouchLeft = true;
+                    break;
+                case "Right":
+                    isTouchRight = true;
+                    break;
+            }
+        }
+
+        else if(collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "EnemyBullet")
+        {
+            if (isHit)
+                return;
+
+            isHit = true;
+            life--;
+            gameManager.UpdateLifeIcon(life);
+            power = 1;
+
+            if(life == 0)
+            {
+                gameManager.GameOver();
+            }
+            else
+            {
+                gameManager.RespawnPlayer();
+            }
+            gameObject.SetActive(false);
+        }
+
+        else if(collision.gameObject.tag == "Item")
+        {
+            Item item = collision.gameObject.GetComponent<Item>();
+            switch (item.type)
+            {
+                case "Coin":
+                    score += 1000;
+                    break;
+                case "Power":
+                    if (power == maxPower)
+                        score += 500;
+                    else
+                        power++;
+                    break;
+                case "Boom":
+                    if (boom == maxBoom)
+                        score += 500;
+                    else
+                    {
+                        boom++;
+                        gameManager.UpdateBoomIcon(boom);
+                    }
+                    break;
+            }
+            collision.gameObject.SetActive(false);
+        }
+    }
+
+    void OffBoomEffect()
+    {
+        boomEffect.SetActive(false);
+        isBoomTime = false;
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Border")
+        {
+            switch (collision.gameObject.name)
+            {
+                case "Top":
+                    isTouchTop = false;
+                    break;
+                case "Bottom":
+                    isTouchBottom = false;
+                    break;
+                case "Left":
+                    isTouchLeft = false;
+                    break;
+                case "Right":
+                    isTouchRight = false;
+                    break;
+            }
+        }
+    }
+}
